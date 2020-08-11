1d2ab58ffc311c93825d721a33654abfebeef7f8
Ivy-Walobwa <ivywalobwa@gmail.com>
2020-08-12 02:41:27 +0300

    add README

--- README.md
+++ README.md
@@ -1,16 +1,8 @@
 # avo_expenses
+# under construction🚧
 
 Personal Expenses App
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+#Preview
+<img src="https://user-images.githubusercontent.com/49009293/89958721-406c9980-dc43-11ea-8dad-01820a7dd900.jpg" alt="avo expenses app" width="200"/>
+<img src="https://user-images.githubusercontent.com/49009293/89958726-42cef380-dc43-11ea-9d5e-e38a8c866b92.jpg" alt="avo expenses app" width="200"/>
--- lib/widgets/chart_card.dart
+++ lib/widgets/chart_card.dart
@@ -1,20 +1,33 @@
+import 'package:avo_expenses/models/transaction_data.dart';
 import 'package:flutter/material.dart';
 import 'package:avo_expenses/constants.dart';
+import 'package:provider/provider.dart';
 
 class ChartCard extends StatelessWidget {
+  final String category;
+  ChartCard({this.category});
 
   @override
   Widget build(BuildContext context) {
+
     final height =  MediaQuery.of(context).size.height;
 
-    return Container(
-      width: double.infinity,
-      height: height * 0.4,
-      child: Text('Chart'),
-      decoration: BoxDecoration(
-        color: kgreenColor1,
-        borderRadius: kbottomBorderRadius,
-      ),
+    return Consumer<TransactionData>(
+      builder: (context, transactionData, index){
+        return Container(
+          width: double.infinity,
+          height: height * 0.4,
+          decoration: BoxDecoration(
+            color: kgreenColor1,
+            borderRadius: kbottomBorderRadius,
+          ),
+          child: Row(
+            children: <Widget>[
+
+            ],
+          ),
+        );
+      },
     );
   }
 }
